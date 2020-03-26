fc226641d0ef79693307ccc94968ffb7b215cd36
Max Kelly <maxwill.kelly@gmail.com>
2020-03-26 21:16:35 +0000

    Classes created in C# and minor changes to Design

---
+++ Bank of Kleptocracy/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_of_Kleptocracy
+{
+    class Card
+    {
+        private int accountNumber;
+        private int bankNumber;
+
+        Card()
+        {
+            Random rnd = new Random();
+            accountNumber = rnd.Next(9999);
+            bankNumber = rnd.Next(100);
+        }
+
+        Card(int accountNumber, int bankNumber)
+        {
+            this.accountNumber = accountNumber;
+            this.bankNumber = bankNumber;
+        }
+
+        int getAccountNumber()
+        {
+            return accountNumber;
+        }
+
+        int getBankNumber()
+        {
+            return bankNumber;
+        }
+    }
+}
