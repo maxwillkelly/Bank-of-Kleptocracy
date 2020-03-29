24f30e22f8c6e06c6a877e3c7dd9ec2b1d2846b3
Tadas <saltenis.tadas@gmail.com>
2020-03-29 23:25:49 +0100

    added inconsistent functionality, fix error in Launch.cs

--- Bank of Kleptocracy/Bank.cs
+++ Bank of Kleptocracy/Bank.cs
@@ -13,9 +13,9 @@ namespace Bank_of_Kleptocracy
 {
 
 
-	public partial class Bank : Form
-	{
-		/*
+    public partial class Bank : Form
+    {
+        /*
 		try
 		{
 			sem.WaitOne();
@@ -28,131 +28,113 @@ namespace Bank_of_Kleptocracy
 		}
 		*/
 
-		//* threads control
-		private Semaphore sem = new Semaphore(0, 1);
+        //* threads control
 
-		//* vars
-		private Account[] accounts;
-		public int BankNumber { get; }
+		bool isSemaphored;
+        private Semaphore sem;
 
-		//* initialize this class
-		public Bank()
-		{
-			InitializeComponent();
+        //* vars
+        private Account[] accounts;
+        public int BankNumber { get; }
+
+        //* initialize this class
+        public Bank(bool IsSemaphored)
+        {
+            InitializeComponent();
+			this.isSemaphored = IsSemaphored;
+			if(isSemaphored)
+			{
+                new Semaphore(0, 1);
+			}
         }
 
-		//* Initialize a number of accounts with random data
-		public void InitAccounts(int n)
-		{
-			try
-			{
-				sem.WaitOne();
+        //* Initialize a number of accounts with random data
+        public void InitAccounts(int n)
+        {
+            try
+            {
+                if (isSemaphored)
+                    sem.WaitOne();
 
-				accounts = new Account[n];
-				Random rnd = new Random();
-				for (int i = 0; i < n; i++)
-				{
-					accounts[i] = new Account(rnd);
-				}
-			}
-			finally
-			{
-				sem.Release();
-			}
-		}
+                accounts = new Account[n];
+                Random rnd = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    accounts[i] = new Account(rnd);
+                }
+            }
+            finally
+            {
+                if (isSemaphored)
+                    sem.Release();
+            }
+        }
 
-		//* print all the data of every account
-		public void Print()
-		{
-			try
-			{
-				sem.WaitOne();
+        //* print all the data of every account
+        public void Print()
+        {
+            try
+            {
+                if (isSemaphored)
+                    sem.WaitOne();
 
-				Console.WriteLine("Bank Number: " + BankNumber);
-				foreach (var account in accounts)
-				{
-					account.Print();
-				}
-			}
-			finally
-			{
-				sem.Release();
-			}
-		}
-		
-		//* check if account with passed account number exists
-		public int checkPin(int accNum, string pin)
-		{
-			try
-			{
-				sem.WaitOne();
+                Console.WriteLine("Bank Number: " + BankNumber);
+                foreach (var account in accounts)
+                {
+                    account.Print();
+                }
+            }
+            finally
+            {
+                if (isSemaphored)
+                    sem.Release();
+            }
+        }
 
-				for (int i = 0; i < accounts.Length; i++)
-				{
-					if (accNum == accounts[i].AccountNumber)
-					{
-						if(pin == accounts[i].Pin)
-						{
-							return i; //* account index
-						}
-						else
-						{
-							return -2; //* account found, but invalid pin
-						}
-					}
-				}
+        //* check if account with passed account number exists
+        public int checkPin(int accNum, string pin)
+        {
+            try
+            {
+                if (isSemaphored)
+                    sem.WaitOne();
+
+                for (int i = 0; i < accounts.Length; i++)
+                {
+                    if (accNum == accounts[i].AccountNumber)
+                    {
+                        if (pin == accounts[i].Pin)
+                        {
+                            return i; //* account index
+                        }
+                        else
+                        {
+                            return -2; //* account found, but invalid pin
+                        }
+                    }
+                }
                 return -1; //* account not found
-			}
-			finally
-			{
-				sem.Release();
-			}
-		}
+            }
+            finally
+            {
+                if (isSemaphored)
+                    sem.Release();
+            }
+        }
 
-		//* check account's balance
-		public int balanceCheck(int accNum, string pin)
-		{
-			try
-			{
-				sem.WaitOne();
+        //* check account's balance
+        public int balanceCheck(int accNum, string pin)
+        {
+            try
+            {
+                if (isSemaphored)
+                    sem.WaitOne();
 
-				int accIndex = checkPin(accNum, pin);
-				if (accIndex >= 0)
-				{
-					return accounts[accIndex].Balance; //* account balance
-				}
-				else if(accIndex == -1)
-				{
-					return -1; //* account not found
+                int accIndex = checkPin(accNum, pin);
+                if (accIndex >= 0)
+                {
+                    return accounts[accIndex].Balance; //* account balance
                 }
-				else
-				{
-					return -2; //* account found, but invalid pin
-                }
-			}
-			finally
-			{
-				sem.Release();
-			}
-		}
-
-		//* withdraw from account's balance
-		public int balanceWithdraw(int accNum, string pin, int amount)
-		{
-			try
-			{
-				sem.WaitOne();
-
-				int accIndex = checkPin(accNum, pin);
-				if (accIndex >= 0)
-				{
-					if(amount<=accounts[accIndex].Balance)
-					{
-						accounts[accIndex].Balance -= amount;
-						return 0; //* withdrawal goes through
-					}
-					else return 1; //* not enough balance to withdraw
-				}
                 else if (accIndex == -1)
                 {
                     return -1; //* account not found
@@ -161,31 +143,33 @@ namespace Bank_of_Kleptocracy
                 {
                     return -2; //* account found, but invalid pin
                 }
-			}
-			finally
-			{
-				sem.Release();
-			}
-		}
+            }
+            finally
+            {
+                if (isSemaphored)
+                    sem.Release();
+            }
+        }
 
-		//* deposit into account's balance
-		public int balanceDeposit(int accNum, string pin, int amount)
-		{
-			try
-			{
-				sem.WaitOne();
+        //* withdraw from account's balance
+        public int balanceWithdraw(int accNum, string pin, int amount)
+        {
+            try
+            {
+                if (isSemaphored)
+                    sem.WaitOne();
 
-				int accIndex = checkPin(accNum, pin);
-				if (accIndex >= 0)
-				{
-					if (accounts[accIndex].Balance + amount <= Int32.MaxValue)
-					{
-						accounts[accIndex].Balance += amount;
-						return 0; //* deposit goes through
-					}
-					else return 1; //* deposit exceeds allowed limit (come on, our bank isn't THAT safe...)
-				}
-				else if (accIndex == -1)
+                int accIndex = checkPin(accNum, pin);
+                if (accIndex >= 0)
+                {
+                    if (amount <= accounts[accIndex].Balance)
+                    {
+                        accounts[accIndex].Balance -= amount;
+                        return 0; //* withdrawal goes through
+                    }
+                    else return 1; //* not enough balance to withdraw
+                }
+                else if (accIndex == -1)
                 {
                     return -1; //* account not found
                 }
@@ -193,11 +177,46 @@ namespace Bank_of_Kleptocracy
                 {
                     return -2; //* account found, but invalid pin
                 }
-			}
-			finally
-			{
-				sem.Release();
-			}
-		}
-	}
+            }
+            finally
+            {
+                if (isSemaphored)
+                    sem.Release();
+            }
+        }
+
+        //* deposit into account's balance
+        public int balanceDeposit(int accNum, string pin, int amount)
+        {
+            try
+            {
+                if (isSemaphored)
+                    sem.WaitOne();
+
+                int accIndex = checkPin(accNum, pin);
+                if (accIndex >= 0)
+                {
+                    if (accounts[accIndex].Balance + amount <= Int32.MaxValue)
+                    {
+                        accounts[accIndex].Balance += amount;
+                        return 0; //* deposit goes through
+                    }
+                    else return 1; //* deposit exceeds allowed limit (come on, our bank isn't THAT safe...)
+                }
+                else if (accIndex == -1)
+                {
+                    return -1; //* account not found
+                }
+                else
+                {
+                    return -2; //* account found, but invalid pin
+                }
+            }
+            finally
+            {
+                if (isSemaphored)
+                    sem.Release();
+            }
+        }
+    }
 }
